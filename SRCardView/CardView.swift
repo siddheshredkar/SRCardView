7adac46e016b211ea96de02f557082b97fc827b1
Siddhesh Redkar <siddhesh.redkar@nium.com>
2021-04-30 20:50:18 +0530

    shadow added

--- SRCardView/CardView.swift
+++ SRCardView/CardView.swift
@@ -20,21 +20,20 @@ class CardView: UIView ,HighlightAnimatable{
     }()
 
     @IBInspectable var cornerRadius: CGFloat = 15
-
     @IBInspectable var shadowOffsetWidth: Int = 0
     @IBInspectable var shadowOffsetHeight: Int = 2
     @IBInspectable var shadowColor: UIColor? = UIColor.black
     @IBInspectable var shadowOpacity: Float = 0.3
-//
-//    override func layoutSubviews(){
-//        layer.cornerRadius = cornerRadius
-//        let shadowPath = UIBezierPath(roundedRect: bounds, cornerRadius: cornerRadius)
-//        layer.masksToBounds = false
-//        layer.shadowColor = shadowColor?.cgColor
-//        layer.shadowOffset = CGSize(width: shadowOffsetWidth, height: shadowOffsetHeight);
-//        layer.shadowOpacity = shadowOpacity
-//        layer.shadowPath = shadowPath.cgPath
-//    }
+
+    override func layoutSubviews(){
+        layer.cornerRadius = cornerRadius
+        let shadowPath = UIBezierPath(roundedRect: bounds, cornerRadius: cornerRadius)
+        layer.masksToBounds = false
+        layer.shadowColor = shadowColor?.cgColor
+        layer.shadowOffset = CGSize(width: shadowOffsetWidth, height: shadowOffsetHeight);
+        layer.shadowOpacity = shadowOpacity
+        layer.shadowPath = shadowPath.cgPath
+    }
     
     override init(frame: CGRect) {
          super.init(frame: frame)
