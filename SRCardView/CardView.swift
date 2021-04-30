2bf32475c7acb5c2652aad4e7f1a58a9ecc72549
Siddhesh Redkar <siddhesh.redkar@nium.com>
2021-04-30 20:37:28 +0530

    shadow removed

--- SRCardView/CardView.swift
+++ SRCardView/CardView.swift
@@ -25,16 +25,16 @@ class CardView: UIView ,HighlightAnimatable{
     @IBInspectable var shadowOffsetHeight: Int = 2
     @IBInspectable var shadowColor: UIColor? = UIColor.black
     @IBInspectable var shadowOpacity: Float = 0.3
-
-    override func layoutSubviews(){
-        layer.cornerRadius = cornerRadius
-        let shadowPath = UIBezierPath(roundedRect: bounds, cornerRadius: cornerRadius)
-        layer.masksToBounds = false
-        layer.shadowColor = shadowColor?.cgColor
-        layer.shadowOffset = CGSize(width: shadowOffsetWidth, height: shadowOffsetHeight);
-        layer.shadowOpacity = shadowOpacity
-        layer.shadowPath = shadowPath.cgPath
-    }
+//
+//    override func layoutSubviews(){
+//        layer.cornerRadius = cornerRadius
+//        let shadowPath = UIBezierPath(roundedRect: bounds, cornerRadius: cornerRadius)
+//        layer.masksToBounds = false
+//        layer.shadowColor = shadowColor?.cgColor
+//        layer.shadowOffset = CGSize(width: shadowOffsetWidth, height: shadowOffsetHeight);
+//        layer.shadowOpacity = shadowOpacity
+//        layer.shadowPath = shadowPath.cgPath
+//    }
     
     override init(frame: CGRect) {
          super.init(frame: frame)
